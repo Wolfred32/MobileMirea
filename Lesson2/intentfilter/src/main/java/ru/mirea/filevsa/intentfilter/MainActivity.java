f84b571c7c174f68c5b743ba458b6a43890d20a3
Fileso <64396961+Wolfred32@users.noreply.github.com>
2024-03-11 22:55:40 +0300

    add les2

---
+++ Lesson2/intentfilter/src/main/java/ru/mirea/filevsa/intentfilter/MainActivity.java
@@ -0,0 +1,40 @@
+package ru.mirea.filevsa.intentfilter;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+import android.net.Uri;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    public void callWeb(View view){
+        Uri address = Uri.parse("https://www.mirea.ru/");
+        Intent openLinkIntent = new Intent(Intent.ACTION_VIEW, address);
+        startActivity(openLinkIntent);
+    }
+
+    public void sendInfo(View view){
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_SUBJECT, "MIREA");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, "ФИЛЁВ СЕРГЕЙ АЛЕКСАНДРОВИЧ");
+        startActivity(Intent.createChooser(shareIntent, "МОИ ФИО"));
+    }
+}
